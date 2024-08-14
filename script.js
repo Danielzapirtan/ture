1d63bf766e1533699a688eb4f3f07135434085ba
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2024-08-14 15:25:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function generateCalendar() {
   month = monthSelector.value - 1;
 	if (!year || !month)
 		return;
-	if (year < 1000 || year > 9999 || month < 0 || month > 11)
+	if (year < 1000 || year > 9999 || month > 11)
 		return;
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const firstDay = (new Date(year, month, 1).getDay() + 6) % 7;
