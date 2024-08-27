a610b51eded113e3919d220aac42fb0a6b2d2e2d
Antoniu-Daniel Zăpîrțan <antoniudanielzapirtan@gmail.com>
2024-08-27 23:32:43 +0000

    indent

--- script.js
+++ script.js
@@ -4,122 +4,122 @@ let month = new Date().getMonth();
 const yearButtons = document.querySelectorAll('.year-buttons button');
 let iear = currentYear;
 yearButtons.forEach(button => {
-	button.textContent = iear;
-	iear++;
+  button.textContent = iear;
+  iear++;
 });
 const monthNames = [
-	"ian",
-	"feb",
-	"mar",
-	"apr",
-	"mai",
-	"iun",
-	"iul",
-	"aug",
-	"sep",
-	"oct",
-	"noi",
-	"dec"
+  "ian",
+  "feb",
+  "mar",
+  "apr",
+  "mai",
+  "iun",
+  "iul",
+  "aug",
+  "sep",
+  "oct",
+  "noi",
+  "dec"
 ];
 
 function highlight(button1) {
-	button1.classList.add("highlight");
+  button1.classList.add("highlight");
 }
 
 function lowlight(buttons) {
-	buttons.forEach(button1 => {
-		button1.classList.remove("highlight");
-	});
+  buttons.forEach(button1 => {
+    button1.classList.remove("highlight");
+  });
 }
 
 const monthButtons = document.querySelectorAll('.month-buttons button');
 const monthName = monthNames[month];
 monthButtons.forEach(button => {
-	if (monthName === button.textContent) {
-		lowlight(monthButtons);
-		highlight(button);
-	}
+  if (monthName === button.textContent) {
+    lowlight(monthButtons);
+    highlight(button);
+  }
 });
 
 yearButtons.forEach(button => {
-	const year1 = parseInt(button.textContent);
-	if (year1 == currentYear) {
-		lowlight(yearButtons);
-		highlight(button);
-	}
+  const year1 = parseInt(button.textContent);
+  if (year1 == currentYear) {
+    lowlight(yearButtons);
+    highlight(button);
+  }
 });
 
 monthButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		const monthName = button.textContent;
-		let ix = 0;
-		monthNames.forEach(currMonthName => {
-			if (monthName == currMonthName) {
-				month = ix;
-			}
-			ix++;
-		});
-		lowlight(monthButtons);
-		highlight(button);
-		updateCalendar();
-	});
+  button.addEventListener('click', () => {
+    const monthName = button.textContent;
+    let ix = 0;
+    monthNames.forEach(currMonthName => {
+      if (monthName == currMonthName) {
+        month = ix;
+      }
+      ix++;
+    });
+    lowlight(monthButtons);
+    highlight(button);
+    updateCalendar();
+  });
 });
 
 yearButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		year = parseInt(button.textContent);
-		lowlight(yearButtons);
-		highlight(button);
-		updateCalendar();
-	});
+  button.addEventListener('click', () => {
+    year = parseInt(button.textContent);
+    lowlight(yearButtons);
+    highlight(button);
+    updateCalendar();
+  });
 });
 
 updateCalendar();
 
 function updateCalendar() {
-	const daysInMonth = new Date(year, month + 1, 0).getDate();
-	const firstDay = (new Date(year, month, 1).getDay() + 6) % 7;
-	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDay = (new Date(year, month, 1).getDay() + 6) % 7;
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
-	let tura = 4;
+  let tura = 4;
 
-	if (urlParams.has("tura")) {
-		const turaValue = urlParams.get("tura");
-		if (
-			!isNaN(turaValue) &&
-			parseInt(turaValue) >= 1 &&
-			parseInt(turaValue) <= 4
-		) {
-			tura = parseInt(turaValue);
-			if (tura % 2 === 0) tura = 6 - tura;
-		}
-	}
-	const date1 = new Date(year, month, 1);
-	const date0 = new Date(2024, 0, 1);
-	let doy = Math.ceil((date1 - date0) / 86400000);
-	let calendarHTML = `<table><tr><th>lun</th><th>mar</th><th>mie</th><th>joi</th><th>vin</th><th>sâm</th><th>dum</th></tr><tr>`;
-	let dayCount = 1;
-	for (let i = 0; i < 42; i++) {
-		if (i >= firstDay && dayCount <= daysInMonth) {
-			let dt = (doy + tura) % 4;
-			let doyClass = `doy${dt}`;
-			calendarHTML += `<td class="${doyClass}">${dayCount}</td>`;
-			doy++;
-			dayCount++;
-		} else {
-			calendarHTML += `<td></td>`;
-		}
+  if (urlParams.has("tura")) {
+    const turaValue = urlParams.get("tura");
+    if (
+      !isNaN(turaValue) &&
+      parseInt(turaValue) >= 1 &&
+      parseInt(turaValue) <= 4
+    ) {
+      tura = parseInt(turaValue);
+      if (tura % 2 === 0) tura = 6 - tura;
+    }
+  }
+  const date1 = new Date(year, month, 1);
+  const date0 = new Date(2024, 0, 1);
+  let doy = Math.ceil((date1 - date0) / 86400000);
+  let calendarHTML = `<table><tr><th>lun</th><th>mar</th><th>mie</th><th>joi</th><th>vin</th><th>sâm</th><th>dum</th></tr><tr>`;
+  let dayCount = 1;
+  for (let i = 0; i < 42; i++) {
+    if (i >= firstDay && dayCount <= daysInMonth) {
+      let dt = (doy + tura) % 4;
+      let doyClass = `doy${dt}`;
+      calendarHTML += `<td class="${doyClass}">${dayCount}</td>`;
+      doy++;
+      dayCount++;
+    } else {
+      calendarHTML += `<td></td>`;
+    }
 
-		if (i % 7 === 6 && dayCount <= daysInMonth) {
-			calendarHTML += `</tr><tr>`;
-		}
+    if (i % 7 === 6 && dayCount <= daysInMonth) {
+      calendarHTML += `</tr><tr>`;
+    }
 
-		if (dayCount > daysInMonth && i % 7 == 6) {
-			break;
-		}
-	}
+    if (dayCount > daysInMonth && i % 7 == 6) {
+      break;
+    }
+  }
 
-	calendarHTML += `</tr></table>`;
-	document.getElementById("calendarContainer").innerHTML = calendarHTML;
+  calendarHTML += `</tr></table>`;
+  document.getElementById("calendarContainer").innerHTML = calendarHTML;
 }
